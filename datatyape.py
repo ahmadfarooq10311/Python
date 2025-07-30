aaa781f86a17c1a3b5cee8a57c9cdad8f91e3722
ahmadfarooq10311 <151871099+ahmadfarooq10311@users.noreply.github.com>
2025-07-30 12:30:44 +0500

    Add files via upload

---
+++ datatyape.py
@@ -0,0 +1,12 @@
+a = 5           # a is a integer
+b = 8.9         # b is a float 
+c = "Ahmad"     # C is a string 
+d = False       # d is a Boolean variable 
+e = None        # e is a none variable datatype is ma kuch nhi ha 
+
+# @abc = "ahn"  ya nhi chly ga Q k is ma special  character ha 
+
+
+# hm ya digit sy shuru kr skty hain , num sy shuru kr skty hain , integer sy shuru kr skty hin , lkn hum kabi b kisi special character sy shuru nhi kr skty
+
+
