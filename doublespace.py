b3bbf2150e73757ee851c156a2e85091a33e6c03
ahmadfarooq10311 <151871099+ahmadfarooq10311@users.noreply.github.com>
2025-07-30 12:31:12 +0500

    Add files via upload

---
+++ doublespace.py
@@ -0,0 +1,6 @@
+name  = input("enter your name")
+skol  = "Agr is ma double space ho ga to ya  positive ma reply dy ga.. agr double space nhi ho ga to negative ma reply dy ga"
+print(name.find("  "))
+print(skol.find("to ya "))
+
+# is ma hum ny find kiya ha double space... agr hmari line ma double... hum ny is k andr letter o find kiya ha... 
